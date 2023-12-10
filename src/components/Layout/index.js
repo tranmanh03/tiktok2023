7b8456d52eba5ce3c88afd76a6fbcfd1bc4dd98a
tranmanh03 <tranmanh200305@gmail.com>
2023-12-10 10:36:01 +0700

    Dung base lan 2

---
+++ src/components/Layout/index.js
@@ -0,0 +1,2 @@
+export { default as DeafaultLayout } from './DefaultLayout';
+export { default as HeaderOnly } from './HeaderOnly';
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+//Public routes
+const PublicRoute = [
+  { path: '/', element: Home },
+  { path: '/following', element: Following },
+  { path: '/profile', element: Profile },
+  { path: '/upload', element: Upload, layout: HeaderOnly },
+  { path: '/search', element: Search, layout: null },
+];
+
+const PrivateRoute = [];
+
+export { PublicRoute, PrivateRoute };
